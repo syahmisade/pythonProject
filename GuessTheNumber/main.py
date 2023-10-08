a2b53046f6c53223b1c2e6a6ee4b3865c5bf421a
Syahmi <amuhamadsade@sheffield.ac.uk>
2023-10-08 17:42:49 +0100

    Added codes for the ccomputer to guess the mystery number

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -13,4 +13,30 @@ def guess(x):
     
     print(f"Yeayy, {random_number} is the correct number!") #the answer of the random_number
 
-guess(10)
+# The computer will try to guess mystery number from the player
+def computer_guess(x):
+    low = 1
+    high = x
+    clue = ''
+
+    #while loop = selagi tak dapat jawapan, selagi tu ulang
+    while clue != 'c': 
+        if low != high:
+            guess = random.randint(low,high) #In random.randint, the low and high value cannot equal. It will give error as the outcome
+        else:
+            guess = low 
+        clue = input(f'Is {guess} too high(H), too low(L), or correct(C)?').lower()
+
+        #kita mahu the computer teka number instead of dia pilih2 number itu ini. We need to change the number guessing bounderies based on the hint given 
+        if clue == 'h':
+            high = guess - 1
+        elif clue == 'l':
+            low = guess + 1
+    
+    print(f"The computer manage to guess number, {guess}, correctly!")
+
+
+
+
+#guess(10)
+computer_guess(10)
